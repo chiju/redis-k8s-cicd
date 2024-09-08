87aad66889fc09c3ac21eb51c15b70df1eedbfad
chiju <chijumelveettil@gmail.com>
2024-09-08 11:14:40 +0200

    updating unique name for terraform state file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 # Configure the S3 backend for Terraform state and locking
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "unzer_test-terraform-state-bucket"
     key            = "terraform/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock-table"
