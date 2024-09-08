17bc930ee9a897c6eeae10e1a82532804ad9b968
chiju <chijumelveettil@gmail.com>
2024-09-08 11:19:26 +0200

    updated bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 # Configure the S3 backend for Terraform state and locking
 terraform {
   backend "s3" {
-    bucket         = "unzer_test-terraform-state-bucket"
+    bucket         = "unzer-test-terraform-state-bucket"
     key            = "terraform/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock-table"
